97dbc60a05a325e65850e8bc134bc4c5e8c45187
Jared Coleman <jaredraycoleman@gmail.com>
2022-10-20 09:23:27 -0700

    Adds initial pattern detection

--- main.py
+++ main.py
@@ -1,11 +1,13 @@
-import random
-import time
+import json
+import pathlib
 from functools import lru_cache
 from hashlib import sha256
-from typing import List, Set
+from typing import Dict, Generator, List, Optional, Set, Tuple, Union
 
-import dask
 import networkx as nx
+import numpy as np
+from matplotlib import cm
+from matplotlib import pyplot as plt
 
 
 def top_down_color_hash(color: str, parent_top_down_color_hashes: Set[str]) -> str:
@@ -113,27 +115,141 @@ def rec_set_color_hashes(graph: nx.DiGraph) -> nx.DiGraph:
         graph.nodes[node]["color_hash"] = node_hash(node)
 
 
-def random_dag() -> nx.DiGraph:
-    """Generate a random DAG.
-
-    Returns:
-        nx.DiGraph: The random DAG
-    """
+def load_dag_from_json(path: Union[pathlib.Path, str]) -> nx.DiGraph:
+    path = pathlib.Path(path)
+    data = json.loads(path.read_text())
     graph = nx.DiGraph()
-    graph.add_nodes_from(range(10000))
-    for node in graph.nodes:
-        graph.nodes[node]["color"] = random.choice(["red", "green", "blue", "yellow", "purple", "orange", "pink", "black", "white"])
-    for node in graph.nodes:
-        for dst in range(node+1, 100):
-            if random.random() < 0.2:
-                graph.add_edge(node, dst)
-    assert(nx.is_directed_acyclic_graph(graph))
+
+    for task in data['workflow']['tasks']:
+        graph.add_node(task['name'], color=task['category'])
+    
+    for task in data['workflow']['tasks']:
+        for parent in task['parents']:
+            graph.add_edge(parent, task['name'])
+    
     return graph
 
-def main():
-    graph = random_dag()
-    print(f"Graph with {graph.number_of_nodes()} nodes and {graph.number_of_edges()} edges")
+def load_dags_from_json(path: Union[pathlib.Path, str]) -> List[nx.DiGraph]:
+    return [
+        load_dag_from_json(json_path)
+        for json_path in pathlib.Path(path).glob("*.json")
+    ]
 
+def draw_workflow(workflow: nx.DiGraph, 
+                  color_attr: Optional[str] = None,
+                  ax: Optional[plt.Axes] = None) -> None:
+    """Draw workflow DAG
+    
+    """
+    if ax is None:
+        fig, ax = plt.subplots(figsize=(10, 10))
+
+    if color_attr is not None:
+        color_idx = {color: i for i, color in enumerate(sorted(set(nx.get_node_attributes(workflow, color_attr).values())))}
+        cmap = cm.get_cmap('rainbow', len(color_idx))
+        node_color = [cmap(color_idx[workflow.nodes[node][color_attr]]) for node in workflow.nodes]
+    else:
+        node_color = ['black' for task in workflow.nodes]
+    pos = nx.nx_agraph.pygraphviz_layout(workflow, prog='dot')
+    nx.draw(
+        workflow, pos, ax=ax, 
+        with_labels=False, 
+        node_color=node_color
+    )
+    if color_attr is not None:
+        for col, i in color_idx.items():
+            plt.scatter([],[], c=[cmap(i)], label=col)
+    return ax.get_figure(), ax
+
+def get_root_idx(paths: List[List[str]]) -> int:
+    """Get the index of the root node in the paths.
+
+    Args:
+        paths (List[List[str]]): The paths
+
+    Returns:
+        int: The index of the root node in the paths
+    """
+    min_len = min(len(path) for path in paths)
+    arr = np.array([path[:min_len] for path in paths])
+    return np.argmax(np.all(arr == arr[0, :], axis=0))
+
+def get_patterns(paths: List[List[str]]) -> Generator[List[str], None, None]:
+    """Get the patterns from the paths.
+
+    Args:
+        paths (List[List[str]]): The paths
+
+    Yields:
+        List[str]: The patterns
+    """
+    root_idx = get_root_idx(paths)
+    for i in range(len(paths)):
+        yield paths[i][root_idx:]
+        paths[i] = paths[i][root_idx+1:] # trim path
+
+    # group paths by commmon first element
+    path_groups: Dict[str, List[str]] = {}
+    for path in paths:
+        if len(path) > 0:
+            path_groups.setdefault(path[0], []).append(path)
+    
+    # recurse on each group
+    for group in path_groups.values():
+        if len(group) > 1:
+            yield from get_patterns(group)
+    
+def task_func(task: str, color_hash: str, in_messages: List[Tuple[str, List[str]]]) -> Tuple[List[str], Tuple[str, List[str]]]:
+    """
+
+    Args:
+        in_messages (List[Tuple[str, List[str]]]): List of tuples of the form (color_hash, [critical_path])
+    
+    Returns:
+        Tuple[str, List[str]]: Tuple of the form (color_hash, [critical_path])
+    """
+    # group messages by color hash
+    color_hash_groups = {}
+    for color_hash, critical_path in in_messages:
+        if color_hash not in color_hash_groups:
+            color_hash_groups[color_hash] = []
+        color_hash_groups[color_hash].append(critical_path)
+
+    patterns = []
+    for color_hash, critical_paths in color_hash_groups.items():
+        # get the patterns from the critical paths
+        if len(critical_paths) > 1:
+            patterns.extend(get_patterns(critical_paths))
+    
+    paths = [path for _, path in in_messages]
+    root_idx = get_root_idx(paths)
+    critical_path = [*paths[0][:root_idx+1], task]
+    return patterns, (color_hash, critical_path)
+
+def find_patterns(graph: nx.DiGraph):
+    # compute color hashes
+    set_color_hashes(graph)
+    top_sort = list(nx.topological_sort(graph))
+    messages = {}
+    for node in top_sort:
+        patterns, msg = task_func(node, graph.nodes[node]["color_hash"], messages.get(node, [('SRC', ['SRC'])]))
+        for child in graph.successors(node):
+            if child not in messages:
+                messages[child] = []
+            messages[child].append(msg)
+        for pattern in patterns:
+            print(f"Pattern: {pattern}")
+
+thisdir = pathlib.Path(__file__).parent.resolve()
+def main():
+    graph = load_dag_from_json(thisdir.joinpath('pegasus-instances', 'montage', 'chameleon-cloud', 'montage-chameleon-2mass-01d-001.json'))
+    graph = set_color_hashes(graph)
+    find_patterns(graph)
+
+    fig, ax = draw_workflow(graph, color_attr="color_hash")
+    plt.legend()
+    plt.show()
+    
 
 if __name__ == "__main__":
     main()
